b39ef2d6d4243769ac59dd86a1745d277008dbeb
klin <klin@sdsc.edu>
2023-12-22 10:52:12 -0800

    add milliseconds component to the log item
    add milliseconds component to the log item

--- src/data_loader/data_loader_daemon.py
+++ src/data_loader/data_loader_daemon.py
@@ -30,7 +30,7 @@ from src.data_loader.data_loader_factory import DataLoaderFactory
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s UTC [%(process)d] %(levelname)s Data Loader %(message)s',
+    format='%(asctime)s.%(msecs)03d UTC [%(process)d] %(levelname)s Data Loader %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
